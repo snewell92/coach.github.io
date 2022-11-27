5861c27faedada26b246843db7adb2c55d192820
Sean Newell <sean@thenewells.us>
2022-11-27 18:57:27 +0000

    Add bounce animation to the fingies on hover

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,34 @@ module.exports = {
       mid: "#AAB4C6",
       light: "#E5FEFF"
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        "bounce-left": {
+          '0%, 100%': {
+            transform: 'translateX(0%)',
+            aimationTimingFunction: 'cubic-bezier(0.8,0,1,1)'
+          },
+          '50%': {
+            transform: 'translateX(-25%)',
+            aimationTimingFunction: 'cubic-bezier(0,0,0.2,0)'
+          },
+        },
+        "bounce-right": {
+          '0%, 100%': {
+            transform: 'translateX(0%)',
+            aimationTimingFunction: 'cubic-bezier(0.8,0,1,1)'
+          },
+          '50%': {
+            transform: 'translateX(25%)',
+            aimationTimingFunction: 'cubic-bezier(0,0,0.2,0)'
+          },
+        },
+      },
+      animation: {
+        'bounce-left': 'bounce-left 1s linear infinite',
+        'bounce-right': 'bounce-right 1s linear infinite'
+      }
+    },
   },
   plugins: [],
 }
