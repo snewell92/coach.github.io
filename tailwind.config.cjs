09156daf442a1f7e13304d82866f10df57a4023d
Sean Newell <sean@thenewells.us>
2022-11-27 17:03:12 +0000

    Added a theme and initial stylings
    Nothing too fancy, just some quick colours
    from colorhub.app and stylings, and emojis... ofc

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,ts,tsx}'],
+  theme: {
+    colors: {
+      brand: "#87E4F7",
+      dark: "#13485C",
+      mid: "#AAB4C6",
+      light: "#E5FEFF"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
